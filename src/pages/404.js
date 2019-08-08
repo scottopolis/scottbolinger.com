de954875d1d1a3e4f884f4e7dccde71bffa2c565
Scott Bolinger <designbyscott@gmail.com>
2019-08-08 15:03:01 -0600

    fix build error

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import Layout from "../components/Layout"
-import Seo from "../components/Seo"
+import Seo from "../components/SEO"
 
 const NotFoundPage = ({ location }) => (
   <Layout location={{ location }}>
