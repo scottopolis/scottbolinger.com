3ee6086a8b3ba941129456ec699e7368bb4750f7
Scott Bolinger <designbyscott@gmail.com>
2019-08-10 20:12:50 -0600

    hide empty comments

--- src/components/PostComments.js
+++ src/components/PostComments.js
@@ -3,6 +3,9 @@ import moment from 'moment/moment'
 
 const PostComments = ({ post }) => {
 
+    if( !post.comments || !post.comments.nodes.length )
+    return(<></>);
+
   return (
       <div
         id="post-comments"
@@ -10,7 +13,7 @@ const PostComments = ({ post }) => {
       >
         <h4 className="mb-6">Comments</h4>
         { post.comments.nodes && post.comments.nodes.map( 
-            comment => <div key={comment.id} className="p-4 rounded border mb-6 bg-gray-100 overflow-hidden">
+        comment => <div key={comment.id} className="p-4 rounded border mb-6 bg-gray-100 overflow-hidden">
                 <h5 className="mb-0">{comment.author.name}</h5>
                 <p className="text-gray-500 text-xs">{moment(comment.date).format(`MMMM D, YYYY`)}</p>
                 <div 
