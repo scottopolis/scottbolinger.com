052e9ce921ea4f97c773fb298e0d4002b077f299
Scott Bolinger <designbyscott@gmail.com>
2019-08-17 09:27:30 -0600

    unique config

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ const config = {
   wordPressUrl: `https://designbyscott.wpengine.com`,
   siteUrl: `https://scottbolinger.com`,
   dynamicComments: true,
-  gaTrackingId: '',
-  mailChimpEndpoint: '',
+  gaTrackingId: 'UA-6044974-7',
+  mailChimpEndpoint: 'https://hollerwp.us16.list-manage.com/subscribe/post?u=d9d0193288fd8270922c02b01&amp;id=8aefa1becd',
 }
 
 module.exports = config
